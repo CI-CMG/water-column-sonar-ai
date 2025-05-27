277069d894feb6b3b1b577b53220fc107b75c3fb
oftfrfbf <rudyklucik@gmail.com>
2025-05-27 14:22:40 -0600

    removing packages

--- water_column_sonar_ai/__init__.py
+++ water_column_sonar_ai/__init__.py
@@ -1,13 +1,7 @@
 from __future__ import absolute_import
 
-from . import aws, cruise, geometry, index, model, processing, utility
+from . import processing
 
 __all__ = [
-    "aws",
-    "cruise",
-    "geometry",
-    "index",
-    "model",
     "processing",
-    "utility",
 ]
